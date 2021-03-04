1c876c8acb1d16635657527d10df60455d97c310
Jordi Planes <jordi.planes@gmail.com>
2021-03-04 16:03:21 +0100

    Update crear-grafica.py

--- crear-grafica.py
+++ crear-grafica.py
@@ -1,15 +1,22 @@
 
+def multiplicacio(x,y):
+   result = 0
+   return result
+
+def calcular_temps():
+    import timeit
+    temps = []
+    for x in range(0,200,10):
+        temps.append( (x, timeit.timeit("multiplicacio("+str(x)+",100)",
+            setup="from __main__ import multiplicacio")) )
+    return temps
+
 import matplotlib.pyplot as plt
-import sys
 
-x_list = []
-y_list = []
-for line in open(sys.argv[1]):
-	x, y = line.split()
-	x = int(x)
-	y = float(y)
-	x_list.append(x)
-	y_list.append(y)
+def crear_grafica( x_list, y_list ):
+    plt.scatter(x_list, y_list)
+    plt.show()
 
-plt.scatter(x_list, y_list)
-plt.show()
+if __name__ == '__main__':
+    temps = calcular_temps()
+    crear_grafica(*map(list, zip(*temps)))
